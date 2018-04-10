0665e9bb19fe0002ff0ec684dc2bbc9e998ca9b5
Aaron Nech <aaronnech@gmail.com>
2018-04-09 21:03:46 -0700

    fixed

--- Singleton/SceneCache.gd
+++ Singleton/SceneCache.gd
@@ -3,10 +3,10 @@ extends Node
 var cache = {}
 
 func cache_scene(name, scene):
-	cache[name] = scene
+	# cache[name] = scene
 	pass
 	
 func get_scene(name):
-	if name in cache:
-	  return cache[name]
+	# if name in cache:
+	#  return cache[name]
 	return null
