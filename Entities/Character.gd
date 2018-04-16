1ce5cb8a3d0ca23bd205d09e5ae8bf972d360a62
Connor O'Kinsella <connorokinsella@gmail.com>
2018-04-15 17:21:38 -0700

    movement

--- Entities/Character.gd
+++ Entities/Character.gd
@@ -1,29 +1,57 @@
 extends Area2D
 
+var tile_size = 32
+var can_move = true
+var facing = 'down'
+var moves = {'right': Vector2(1, 0),
+              'left': Vector2(-1, 0),
+			  'up': Vector2(0, -1),
+			  'down': Vector2(0, 1)}
+
+var raycasts = {'right': $ray_right,
+              'left': $ray_left,
+			  'up': $ray_up,
+			  'down': $ray_down}
+			
+func move(direction):
+	facing = direction
+	if get_node(raycasts[facing]).is_colliding():
+        return false
+	can_move = false
+	$AnimationPlayer.play(facing)
+	$MoveTween.interpolate_property(self, 'position', position,
+	                                position + moves[facing] * tile_size, 0.8,
+									Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+	$MoveTween.start()
+	return true
+			
 
 export (int) var SPEED
 
-func _ready():
-	$HitBox.disabled = false
+#func _ready():
+#	$HitBox.disabled = false
 
-func _process(delta):
-    var velocity = Vector2() # the player's movement vector
-	# Key events in _process will be called continously
-    if Input.is_action_pressed("ui_accept"):
-        $Inventory.use_active_item()
-    if Input.is_action_pressed("ui_right"):
-        velocity.x += 1
-    if Input.is_action_pressed("ui_left"):
-        velocity.x -= 1
-    if Input.is_action_pressed("ui_down"):
-        velocity.y += 1
-    if Input.is_action_pressed("ui_up"):
-        velocity.y -= 1
-    if velocity.length() > 0:
-        velocity = velocity.normalized() * SPEED
-    position += velocity * delta
+#func _process(delta):
+#    var velocity = Vector2() # the player's movement vector
+#	# Key events in _process will be called continously
+#    if Input.is_action_pressed("ui_accept"):
+#        $Inventory.use_active_item()
+#    if Input.is_action_pressed("ui_right"):
+#        velocity.x += 1
+#    if Input.is_action_pressed("ui_left"):
+#        velocity.x -= 1
+#    if Input.is_action_pressed("ui_down"):
+#        velocity.y += 1
+#    if Input.is_action_pressed("ui_up"):
+#        velocity.y -= 1
+#    if velocity.length() > 0:
+#        velocity = velocity.normalized() * SPEED
+#    position += velocity * delta
+#
+#func _input(event):
+#	# These key events only get called once
+#	if(event.is_action_pressed("drop")):
+#		$Inventory.drop_active_item()
 
-func _input(event):
-	# These key events only get called once
-	if(event.is_action_pressed("drop")):
-		$Inventory.drop_active_item()
+func _on_MoveTween_tween_completed(object, key):
+	can_move = true
--- Entities/Player.gd
+++ Entities/Player.gd
@@ -1,29 +1,7 @@
-extends Area2D
-
-
-export (int) var SPEED
-
-func _ready():
-	$HitBox.disabled = false
+extends "res://Entities/Character.gd"
 
 func _process(delta):
-    var velocity = Vector2() # the player's movement vector
-	# Key events in _process will be called continously
-    if Input.is_action_pressed("ui_accept"):
-        $Inventory.use_active_item()
-    if Input.is_action_pressed("ui_right"):
-        velocity.x += 1
-    if Input.is_action_pressed("ui_left"):
-        velocity.x -= 1
-    if Input.is_action_pressed("ui_down"):
-        velocity.y += 1
-    if Input.is_action_pressed("ui_up"):
-        velocity.y -= 1
-    if velocity.length() > 0:
-        velocity = velocity.normalized() * SPEED
-    position += velocity * delta
-
-func _input(event):
-	# These key events only get called once
-	if(event.is_action_pressed("drop")):
-		$Inventory.drop_active_item()
+	if can_move:
+		for dir in moves.keys():
+			if Input.is_action_pressed(dir):
+				move(dir)
